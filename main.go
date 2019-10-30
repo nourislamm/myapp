2f031da91d6047d4fe60b0e9ddfb8c4539650df7
acml-w19 <55441588+acml-w19@users.noreply.github.com>
2019-10-30 12:59:17 +0200

    Update main.go

--- main.go
+++ main.go
@@ -1,8 +1,41 @@
 package main
 
-import("github.com/common-nighthawk/go-figure")
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	dbUser     = "root"
+	dbPassword = "secret"
+	dbName     = "myapp"
+)
 
 func main() {
-  myFigure := figure.NewFigure("Hii World", "", true)
-  myFigure.Print()
+	dbInfo := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable",
+		dbUser, dbPassword, dbName)
+	db, err := sql.Open("postgres", dbInfo)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	rows, err := db.Query("SELECT * FROM users")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var (
+			id   int
+			name string
+		)
+
+		rows.Scan(&id, &name)
+
+		log.Printf("%d: %s", id, name)
+	}
 }
