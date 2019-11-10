61f58f9d8cfe1703cf7d3321b5a09b2a8fca2a71
acml-w19 <55441588+acml-w19@users.noreply.github.com>
2019-11-10 20:32:22 +0200

    Update main.go

--- main.go
+++ main.go
@@ -4,33 +4,42 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"net/http"
+	"os"
+
 	_ "github.com/lib/pq"
 )
 
-const(
-DbUser = "uh7iwqw1lvyesf9ojgvs"
-DbPassword = "dfX89QUJcSO8kRXEkpv1"
-DbName = "bzx9lkigjqdafipgmv2a"
-DbHost = "bzx9lkigjqdafipgmv2a-postgresql.services.clever-cloud.com"
-DbPort = "5432"
+var (
+	dbHost     = envOrDefault("MYAPP_DATABASE_HOST", "localhost")
+	dbPort     = envOrDefault("MYAPP_DATABASE_PORT", "5432")
+	dbUser     = envOrDefault("MYAPP_DATABASE_USER", "root")
+	dbPassword = envOrDefault("MYAPP_DATABASE_PASSWORD", "secret")
+	dbName     = envOrDefault("MYAPP_DATABASE_NAME", "myapp")
+
+	webHost = envOrDefault("MYAPP_WEB_HOST", "")
+	webPort = envOrDefault("MYAPP_WEB_PORT", "8080")
+
+	db *sql.DB
 )
 
-func main() {
-    Info := fmt.Sprintf("user=%s password=%s dbname=%s host=%s port=%s sslmode=disable",
-		DbUser, DbPassword, DbName, DbHost, DbPort)
-
-	db, err := sql.Open("postgres", Info)
-	if err != nil {
-		fmt.Print(err)
-		fmt.Print("exit")
+func envOrDefault(key, defaultValue string) string {
+	if value := os.Getenv(key); value != "" {
+		return value
 	}
+	return defaultValue
+}
 
+func myHandler(w http.ResponseWriter, r *http.Request) {
 	rows, err := db.Query("SELECT * FROM users")
 	if err != nil {
-		log.Fatal(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 	defer rows.Close()
 
+	fmt.Fprintln(w, "ID | Name")
+	fmt.Fprintln(w, "---+--------")
 	for rows.Next() {
 		var (
 			id   int
@@ -39,6 +48,23 @@ func main() {
 
 		rows.Scan(&id, &name)
 
-		log.Printf("%d: %s", id, name)
+		fmt.Fprintf(w, "%2d | %s\n", id, name)
 	}
+}
+
+func main() {
+	dbInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		dbHost, dbPort, dbUser, dbPassword, dbName)
+	var err error
+	db, err = sql.Open("postgres", dbInfo)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err = db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+
+	http.HandleFunc("/", myHandler)
+	log.Print("Listening on " + webHost + ":" + webPort + "...")
+	http.ListenAndServe(webHost+":"+webPort, nil)
 }
